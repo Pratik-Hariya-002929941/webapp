33a9b82c3acabc071959307f47d5dcced8e92c43
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-11-16 18:48:04 -0500

    DynamoDB Model and config

---
+++ SpringBootApp/src/main/java/com/example/neu/springbootapp/config/DynamoDbConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.neu.springbootapp.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.example.neu.springbootapp.model.OneTimeToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfiguration {
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(amazonAWSCredentials()))
+                .withRegion(Regions.US_EAST_1).build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
+        init(dynamoDBMapper, client);
+        return dynamoDBMapper;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    }
+
+    public void init(DynamoDBMapper dynamoDBMapper, AmazonDynamoDB client) {
+
+        CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(OneTimeToken.class);
+        tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+        if (TableUtils.createTableIfNotExists(client, tableRequest)) {
+            System.out.println("Table created");
+        }
+
+    }
+}
