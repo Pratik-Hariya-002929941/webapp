2fcce34251f8fe6270173669639c8dce8a72b4b7
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-11-16 19:15:37 -0500

    OneTimeToken Repository and save from user controller

---
+++ SpringBootApp/src/main/java/com/example/neu/springbootapp/repository/OneTimeTokenRepository.java
@@ -0,0 +1,26 @@
+package com.example.neu.springbootapp.repository;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.example.neu.springbootapp.model.OneTimeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OneTimeTokenRepository {
+
+    private DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    public OneTimeTokenRepository(DynamoDBMapper dynamoDBMapper) {
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
+
+    public OneTimeToken getOneTimeToken(String token) {
+        return dynamoDBMapper.load(OneTimeToken.class, token);
+    }
+
+    public OneTimeToken createOneTimeToken(OneTimeToken oneTimeToken) {
+        dynamoDBMapper.save(oneTimeToken);
+        return oneTimeToken;
+    }
+}
